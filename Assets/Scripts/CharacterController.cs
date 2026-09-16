0e14fd70f85d9f1be722fef337e7baeba6b36346
José Vitor A. Oliveira <josevitoraoliveira0@gmail.com>
2026-09-16 14:58:41 -0300

    First commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] float MovementDirection;
+    [SerializeField] float Speed = 7;
+
+    [SerializeField] float JumpStrength = 20f;
+    [SerializeField] bool DoubleJumped = true;
+
+    [SerializeField] float DashStrength = 12f;
+    [SerializeField] float DashDuration = 0.3f;
+    [SerializeField] float DashCooldown = 2f;
+    [SerializeField] bool IsDashing;
+    [SerializeField] bool CanDash = true;
+
+    [SerializeField] bool HasSword = false;
+    [SerializeField] bool IsAttacking = false;
+
+    [SerializeField] Transform AttackPoint;
+    [SerializeField] float AttackRange = 0.5f;
+    [SerializeField] int AttackDamage = 5;
+    [SerializeField] LayerMask EnemyLayers;
+
+
+
+
+
+    [SerializeField] Animator animator;
+
+
+    float FacingDirection = 1f; /*Direita = 1f, Esquerda = -1f*/
+
+
+
+    [SerializeField] Rigidbody2D Rigidbody;
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] LayerMask Ground;
+
+    public void DealDamage()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, EnemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            EnemyAI enemyScript = enemy.GetComponent<EnemyAI>();
+
+            if (enemyScript != null)
+            {
+                enemyScript.TakeDamage(AttackDamage);
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Sword"))
+        {
+            HasSword = true;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovementDirection = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(MovementDirection));
+
+        if (MovementDirection > 0)
+        {
+            FacingDirection = 1f;
+        } 
+        else if (MovementDirection < 0)
+        {
+            FacingDirection = -1f;
+        }
+        animator.SetFloat("FacingDirection", FacingDirection);
+
+
+        if(Input.GetButtonDown("Jump") && (CheckGround() || DoubleJumped))
+        {
+
+            Rigidbody.velocity = new Vector2(Rigidbody.velocity.x, 0f);
+            Rigidbody.AddForce(transform.up * JumpStrength, ForceMode2D.Impulse);
+            DoubleJumped = false;
+
+            if(!CheckGround())
+            {
+                DoubleJumped = false;
+            }
+
+        }
+
+        animator.SetBool("CheckGround", CheckGround());
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && CanDash)
+        {
+            Rigidbody.velocity = new Vector2(0f, Rigidbody.velocity.y);
+            StartCoroutine(Dash());
+        }
+
+        if(Input.GetMouseButtonDown(0) && HasSword && !IsAttacking)
+        {
+            IsAttacking = true;
+            Rigidbody.velocity = new Vector2(0f, Rigidbody.velocity.y);
+            animator.SetTrigger("Attack");
+            
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (IsAttacking)
+        {
+            return;
+        }
+
+        if (IsDashing)
+        {
+            animator.SetBool("IsDashing", IsDashing);
+
+            return;
+        }
+        Rigidbody.velocity = new Vector2(MovementDirection * Speed, Rigidbody.velocity.y);
+        animator.SetBool("IsDashing", IsDashing);
+
+    }
+
+    bool CheckGround()
+    {
+        if(Physics2D.OverlapCircle(GroundCheck.position, .25f, Ground))
+        {
+            DoubleJumped = true;
+            return true;
+            
+        }
+        return false;
+
+    }
+
+    private IEnumerator Dash()
+    {
+        CanDash = false;
+        IsDashing = true;
+
+
+        float OriginalGravity = Rigidbody.gravityScale;
+        Rigidbody.gravityScale = 0f;
+
+        Rigidbody.velocity = new Vector2(DashStrength * FacingDirection, 0f);
+
+        yield return new WaitForSeconds(DashDuration);
+
+        Rigidbody.gravityScale = OriginalGravity;
+
+        float DecelerationTime = 0.25f;
+        float Timer = 0f;
+
+        while (Timer < DecelerationTime)
+        {
+            Timer += Time.deltaTime;
+
+            float PercentComplete = Timer / DecelerationTime;
+
+            float SmoothSpeed = Mathf.Lerp(DashStrength * FacingDirection, 0f, PercentComplete);
+
+            Rigidbody.velocity = new Vector2(SmoothSpeed, Rigidbody.velocity.y);
+
+            yield return null;
+        }
+
+        IsDashing = false;
+        
+        yield return new WaitForSeconds(DashCooldown);
+
+        CanDash = true;
+    }
+
+    public void FinishAttack()
+    {
+        IsAttacking = false;
+    }
+}
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] float EnemySpeed = 4;
+    [SerializeField] float VisionRange;
+    [SerializeField] Transform Player;
+    [SerializeField] Rigidbody2D Rigidbody;
+    [SerializeField] Animator animator;
+    [SerializeField] float JumpStrength = 3;
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] LayerMask Ground;
+
+    [SerializeField] bool TakeHit = false;
+    [SerializeField] bool Dead = false;
+
+
+    [SerializeField] int MaxLife = 10;
+    private int ActualLife;
+
+
+    public void TakeDamage(int damageAmount)
+    {
+        ActualLife -= damageAmount;
+        TakeHit = true;
+        Rigidbody.velocity = new Vector2(0f, Rigidbody.velocity.y);
+        animator.SetTrigger("TakeHit");
+
+        if(ActualLife <= 0)
+        {
+            Dead = true;
+            Rigidbody.velocity = new Vector2(0f, Rigidbody.velocity.y);
+            animator.SetTrigger("Dead");
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        ActualLife = MaxLife;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Distance = Vector2.Distance(transform.position, Player.position);
+
+
+        if (Distance <= VisionRange)
+        {
+
+            bool isEnemyGrounded = CheckGround();
+
+            if (TakeHit)
+            {
+                return;
+            }
+
+            if (Dead)
+            {
+                return;
+            }
+
+            if (Player.position.y > transform.position.y + 1.5f && isEnemyGrounded)
+            {
+                Rigidbody.velocity = new Vector2(Rigidbody.velocity.x, 0f);
+
+                Rigidbody.AddForce(transform.up * JumpStrength, ForceMode2D.Impulse);
+            }
+
+            float Direction = 0f;
+
+            if (Player.position.x > transform.position.x)
+            {
+                Direction = 1f;
+            }
+            else
+            {
+                Direction = -1f;
+            }
+
+            Rigidbody.velocity = new Vector2(Direction * EnemySpeed, Rigidbody.velocity.y);
+            animator.SetFloat("Speed", Mathf.Abs(Rigidbody.velocity.x));
+            animator.SetFloat("Direction", Direction);
+
+
+        }
+
+        
+    }
+
+    bool CheckGround()
+    {
+    if(Physics2D.OverlapCircle(GroundCheck.position, 0.2f, Ground))
+    {
+        return true;
+    }
+    return false;
+    }
+
+    public void FinishHit()
+    {
+        TakeHit = false;
+    }
+
+    public void FinishDeath()
+    {
+        Destroy(gameObject);
+
+        Dead = false;
+
+    }
+}
+
+    
